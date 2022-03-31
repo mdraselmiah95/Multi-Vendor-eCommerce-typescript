4323c50eb272b9fc84d474aadaef08bedff0c10f
mdraselmiah95 <russelroy15@gmail.com>
2022-03-31 13:51:30 +0600

    new folder added

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,7 +1,7 @@
-const publicRoutes = [
-  {
-    path: "/",
-    name: "Home",
-    // component:<Home/>
-  },
-];
+// const publicRoutes = [
+//   {
+//     path: "/",
+//     name: "Home",
+//     // component:<Home/>
+//   },
+// ];
