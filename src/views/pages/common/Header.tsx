2040759e560f16bb19e8699c9aa5b0767fb356bf
mdraselmiah95 <russelroy15@gmail.com>
2022-04-01 14:37:11 +0600

    new code added

--- src/views/pages/common/Header.tsx
+++ src/views/pages/common/Header.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import logo from "../../../assets/icons/daraz-logo.png";
 
 export const Header = () => {
   return (
-    <div className="">
-      <h2>Header</h2>
+    <div className="container mx-auto">
+      <img src={logo} alt="logo" />
     </div>
   );
 };
