0a4b5ad24d83365afbb08367e9c173c946926834
mdraselmiah95 <russelroy15@gmail.com>
2022-04-02 17:15:27 +0600

    new code added

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -11,16 +11,16 @@ const instance: AxiosInstance = axios.create(axiosConfig);
 
 class Request {
   async get(url: string): Promise<AxiosResponse> {
-    return instance.get(url);
+    return instance.get(url).then((res) => res.data);
   }
   async post(url: string, body): Promise<AxiosResponse> {
-    return instance.post(url, body);
+    return instance.post(url, body).then((res) => res.data);
   }
   async update(url: string, body): Promise<AxiosResponse> {
-    return instance.patch(url, body);
+    return instance.patch(url, body).then((res) => res.data);
   }
   async delete(url: string): Promise<AxiosResponse> {
-    return instance.delete(url);
+    return instance.delete(url).then((res) => res.data);
   }
 }
 
