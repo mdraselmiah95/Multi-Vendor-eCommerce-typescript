eeff0f309956e02a31efd08a1665fc4701ab4e6c
mdraselmiah95 <russelroy15@gmail.com>
2022-04-02 15:57:50 +0600

    new code added

--- src/views/pages/common/Main.tsx
+++ src/views/pages/common/Main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../home/Home";
 
+//code-splitting
 export const Main = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={{}} />
         </Routes>
       </BrowserRouter>
     </>
