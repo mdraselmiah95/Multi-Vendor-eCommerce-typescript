97723a62fc46ddb986b3d2dd01bbc2b7c6cb5c1d
mdraselmiah95 <russelroy15@gmail.com>
2022-04-02 17:23:22 +0600

    new code added

---
+++ src/services/Product.service.ts
@@ -0,0 +1,8 @@
+import httpReq from "./http.service";
+
+class ProductService {
+  async getAllProducts(): Promise<any> {
+    const { data } = await httpReq.get("/product");
+    return data;
+  }
+}
